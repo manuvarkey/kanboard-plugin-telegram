f2cbcc4251dcdcb5e5e2cc78403010ba85a801fa
linvinus <linvinus@gmail.com>
2018-02-28 11:29:16 +0300

    new: daemon mode, ./cli plugin:telegram:pool daemon

--- Console/TelegramCommand.php
+++ Console/TelegramCommand.php
@@ -4,6 +4,7 @@ namespace Kanboard\Plugin\Telegram;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 //use Kanboard\Console;
 use Pimple\Container;
 use Symfony\Component\Console\Command\Command;
@@ -28,105 +29,113 @@ use Longman\TelegramBot\Exception\TelegramException;
  */
 class TelegramCommand extends BaseCommand
 {
+    protected $output;
+    
     protected function configure()
     {
         $this
-            ->setName('plugin:telegram:version')
+            ->setName('plugin:telegram:pool')
             ->setDescription('Display Kanboard version')
+            ->addArgument('daemon', InputArgument::OPTIONAL, 'Run as daemon', false);
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $output->writeln("hi from telegram!");
-
-        $apikey = $this->configModel->get('telegram_apikey');
-        $bot_username = $this->configModel->get('telegram_username');
-        $offset = 0+$this->configModel->get('telegram_offset');
-        
-        list($offset, $chat_id, $message, $data) = $this->process_commands($apikey, $bot_username, $offset,$output);
-        
-        if($offset != 0){
+    private function processCallbackQuery($chat_id, $message, $data){
+              
           //ok
-          $this->configModel->save( array('telegram_offset' => $offset) );
+          
           foreach($this->getAllUsersByTelegramChatId($chat_id) as $user){
-            $output->writeln("user_id=".$user['id']." ");
+            $this->output->writeln("user_id=".$user['id']." ");
             $tcmd = explode('/',$data);
             if(count($tcmd)>2){
               switch($tcmd[0]){
                 case 'close':
-                  $output->writeln("got close subtask=".$tcmd[3]." ");
+                  $this->output->writeln("got close subtask=".$tcmd[3]." ");
                   $this->subtaskModel->update(array('id'=>$tcmd[3],'status'=>SubtaskModel::STATUS_DONE));
                   break;
                 case 'work':
-                  $output->writeln("got work subtask=".$tcmd[3]." ");
+                  $this->output->writeln("got work subtask=".$tcmd[3]." ");
                   $this->subtaskModel->update(array('id'=>$tcmd[3],'status'=>SubtaskModel::STATUS_INPROGRESS));
                   break;
               }
             }
           }
-        }else{
-          //error
-        }
     }
 
-    private function process_commands($apikey, $bot_username, $offset,OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-      try
-      {
 
-        // Create Telegram API object
-        $telegram = new TelegramClass($apikey, $bot_username);
+      $this->output=$output;
+      
+      $apikey = $this->configModel->get('telegram_apikey');
+      $bot_username = $this->configModel->get('telegram_username');
+      // Create Telegram API object
+      $telegram = new TelegramClass($apikey, $bot_username);
 
-        $limit=100;
-        $timeout = 1;
-        $response  = Request::getUpdates(
-          [
-          'offset'  => $offset+1,
-          'limit'   => $limit,
-          'timeout' => $timeout,
-          ]
-          );
+      do{
+        $offset = 0+$this->configModel->get('telegram_offset');
+        $output->writeln("hi from telegram! offset=$offset");
 
-        $chat_id="";
-        $message="";
-        $data = "";
 
-        if ($response->isOk()) {
-          //Process all updates
-          /** @var Update $result */
-          //~ $output->writeln("new getCallbackQuery".print_r($response->getResult(),true));
-          foreach ((array) $response->getResult() as $result) {
-            $offset = $result->getUpdateId();
-            
-            if( $result->getMessage() != NULL){
-              $chat_id = $result->getMessage()->getChat()->getId();
-              $message = trim($result->getMessage()->getText());
-              $output->writeln("new message '$message' from '$chat_id'");
-             }elseif($result->getCallbackQuery() != NULL){
-              $q = $result->getCallbackQuery();
-              $data = $q->getData();
-              //$chat_id = $q->getId();
-              $chat_id        = $q->getFrom()->getId();
-              $query_id       = $q->getId();
+        try
+        {
 
-              $message = trim($q->getText());
-              $output->writeln("new getCallbackQuery '$message' from '$chat_id'".print_r($data,true));
-             }
+
+          $limit=100;
+          $timeout = 1;
+          $response  = Request::getUpdates(
+            [
+            'offset'  => $offset+1,
+            'limit'   => $limit,
+            'timeout' => $timeout,
+            ]
+            );
+
+          $chat_id="";
+          $message="";
+          $data = "";
+
+          if ($response->isOk()) {
+            //Process all updates
+            /** @var Update $result */
+            //~ $output->writeln("new getCallbackQuery".print_r($response->getResult(),true));
+            foreach ((array) $response->getResult() as $result) {
+              $offset = $result->getUpdateId();
+              $this->configModel->save( array('telegram_offset' => $offset) );
+              
+              if( $result->getMessage() != NULL){
+                $chat_id = $result->getMessage()->getChat()->getId();
+                $message = trim($result->getMessage()->getText());
+                $output->writeln("new message '$message' from '$chat_id'");
+               }elseif($result->getCallbackQuery() != NULL){
+                $q = $result->getCallbackQuery();
+                $data = $q->getData();
+                //$chat_id = $q->getId();
+                $chat_id        = $q->getFrom()->getId();
+                $query_id       = $q->getId();
+
+                $message = trim($q->getText());
+                $output->writeln("new getCallbackQuery '$message' from '$chat_id'".print_r($data,true));
+
+                $this->processCallbackQuery($chat_id,$message,$data);
+                
+                Request::answerCallbackQuery(['callback_query_id' => $query_id]);
+               }
+            }
+          }else{
+            throw new TelegramException($response->printError(true));
           }
-        }else{
-          throw new TelegramException($response->printError(true));
         }
-      }
-      catch (TelegramException $e)
-      {
-        // log telegram errors
-        error_log($e->getMessage());
-        $this->flash->failure(t('Telegram error: ').$e->getMessage());
-        return 0;//$this->response->redirect($this->helper->url->to('UserViewController', 'integrations', array('user_id' => $user['id'] )), true);
-      }
+        catch (TelegramException $e)
+        {
+          // log telegram errors
+          error_log($e->getMessage());
+          $this->flash->failure(t('Telegram error: ').$e->getMessage());
+          return 0;//$this->response->redirect($this->helper->url->to('UserViewController', 'integrations', array('user_id' => $user['id'] )), true);
+        }
 
-      return array($offset, $chat_id, $message,$data);
+        $this->memoryCache->flush();
+      }while($input->getArgument('daemon')!==false);
     }
 
     public function getAllUsersByTelegramChatId($chat_id){
