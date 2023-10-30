d3558bea757beb21f589df91bab61a2b33f9ab92
Manu Varkey <manuvarkey@gmail.com>
2023-10-30 11:28:07 +0530

    Add comments for proxy URI

--- Template/config/integration.php
+++ Template/config/integration.php
@@ -10,6 +10,7 @@
 
     <?= $this->form->label(t('Telegram proxy server'), 'telegram_proxy') ?>
     <?= $this->form->text('telegram_proxy', $values, array()) ?>
+    <p class="form-help"><?= t('Use proxy URI of the form "http://hostname_or_ip:port" or "http://username:password@hostname_or_ip:port".').'<br/>'.t('For example, "http://username:password@192.168.16.1:10".').'<br/>'.t('(Use "socks5h" instead of "http" for SOCKS5 proxies).') ?></p>
 
     <?= $this->form->hidden('forward_attachments', array('forward_attachments' => 0)) ?>
     <?= $this->form->checkbox('forward_attachments', t('Sent attachments along with notification'), 1, isset($values['forward_attachments']) && $values['forward_attachments'] == 1) ?>    
